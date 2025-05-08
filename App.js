fd67fe08af83a2e6730d3d187f3ec1e89980c858
12ed3e34dw <pvlad8056@gmail.com>
2025-05-08 19:22:57 +0300

    init

--- App.js
+++ App.js
@@ -1,6 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useEffect, useState} from "react";
+import {createStackNavigation} from "@react-native/stack";
+import * as trace_events from "node:trace_events";
 
+
+const UsersScreen = () => {
+
+  const [users,setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/users")
+        .then(response => response.json())
+        .then(data => {setUsers(data)
+          console.log(data)})
+        .catch(error => console.log(error));
+    //После запроса FALSE до этого True
+   setLoading(false);
+  }, [])
+
+
+
+  return (
+      <View style={styles.container}>
+
+      </View>
+  )
+}
+
+
+
+const UserDetailsScreen=()=>{
+
+  return(
+      <View style={styles.container}>
+
+      </View>
+  )
+
+}
+
+
+
+
+
+const Stack=createStackNavigation()
 export default function App() {
   return (
     <View style={styles.container}>
